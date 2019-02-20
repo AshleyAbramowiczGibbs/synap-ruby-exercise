f2960dfe9f2d090c7d418a07ddc087a4359ba587
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2019-02-19 21:48:19 -0600

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    get "/people" => "people#index"
+    get "/people/:id" => "people#show"
+
+
+    get "/organizations" => "people#index"
+    get "/organizations" => "photos#show"
+  end
 end
