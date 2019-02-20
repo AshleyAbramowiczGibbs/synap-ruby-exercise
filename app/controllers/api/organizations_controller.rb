4a5ed63ecb53ba58a8edba7a69b243664cbdb7c4
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2019-02-19 21:51:31 -0600

    set up controllers

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,2 +1,11 @@
 class Api::OrganizationsController < ApplicationController
+  def index
+    @organizations = Organization.all
+    render "index.json.jbuilder"
+  end
+
+  def show
+    @organization = Organization.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
 end
--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,2 +1,11 @@
 class Api::PeopleController < ApplicationController
+  def index
+    @people = Person.all
+    render "index.json.jbuilder"
+  end
+
+  def show
+    @person = Person.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
 end
