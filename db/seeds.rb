21c03ecaaa65d4680aaeb39abcfa44c4441b9b26
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2019-02-19 21:43:51 -0600

    migrate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create(name: "Test Person", job_title:"boss", email_address:"test@email.com", phone: "112-223-2234", organization_id: 1)
+
+Organization.create(name: "Company A", domain:"company.com", phone: "112-223-2234", people_count:40, address_id:1)
+
+Address.create(street: "Apple St", city:"Chicago", state:"IL")
